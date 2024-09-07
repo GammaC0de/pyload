8f5bea8dd798853b926f0ebffc2cdff82559e828
GammaC0de <gammac0de@users.noreply.github.com>
2024-09-08 00:08:00 +0300

    test

--- src/pyload/plugins/downloaders/FilericeCom.py
+++ src/pyload/plugins/downloaders/FilericeCom.py
@@ -11,7 +11,7 @@ class FilericeCom(XFSDownloader):
 
     __pattern__ = r"https?://(?:www\.)?filerice\.com/(?P<ID>\w{12})"
     __config__ = [
-        ("enabled", "bool", "Activated", True),
+        ("enabled", "bool", "Activated", True ),
         ("use_premium", "bool", "Use premium account if available", True),
         ("fallback", "bool", "Fallback to free download if premium fails", True),
         ("chk_filesize", "bool", "Check file size", True),
